8983397a91d1eae7354ed80b3d13d0d8e4135ff4
Rael <135059790+Rael6978@users.noreply.github.com>
2025-07-02 18:06:08 -0300

    Update receba.py
    adicionei a soma

--- receba.py
+++ receba.py
@@ -1,12 +1,12 @@
 line = int(input("digite o número de linhas da matriz: "))
 colunas = int(input("digite o número de colunas da matriz: "))
-matriz = []
+matrizA = []
 
 #recebe valores da matriz
 for cortinas in range(line):
     ordemOK = True
     while ordemOK: #garante que o input repita caso os elementos estejam a mais
-        entrada = input(f"Digite os valores da linha {cortinas+1}, separados por espaço: ")
+        entrada = input(f"Digite os valores da linha {cortinas+1}, da matriz A separados por espaço: ")
         valores = entrada.split()
     
         #verifica se a quantidade de elementos bate com a quantidade de colunas e adiciona os elementos certos na matriz
@@ -14,12 +14,46 @@ for cortinas in range(line):
             linha = []
             for YE in valores:
                 linha.append(int(YE))
-            matriz.append(linha)
+            matrizA.append(linha)
             ordemOK = False
         else:
             ordemOK = True
 
 #printar a matriz
-print("Matriz:")
-for linha in matriz:
+print("Matriz A:")
+for linha in matrizA:
     print(linha)
+
+
+matrizB = []
+
+#recebe valores da matriz B
+for cortinas in range(line):
+    ordemOK = True
+    while ordemOK: #garante que o input repita caso os elementos estejam a mais
+        entrada = input(f"Digite os valores da linha {cortinas+1}, da matriz B separados por espaço: ")
+        valores = entrada.split()
+    
+        #verifica se a quantidade de elementos bate com a quantidade de colunas e adiciona os elementos certos na matriz
+        if len(valores) == colunas:
+            linha = []
+            for YE in valores:
+                linha.append(int(YE))
+            matrizB.append(linha)
+            ordemOK = False
+        else:
+            ordemOK = True
+
+
+#printar a matriz
+print("Matriz B:")
+for linha in matrizB:
+    print(linha)
+
+#soma das matrizes
+for i in range(line):
+    linha_soma = []
+    for j in range(colunas):
+        soma = matrizA[i][j] + matrizB[i][j]
+        linha_soma.append(soma)
+    print(linha_soma)
